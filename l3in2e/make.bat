2c4bfd623b4fb43e83484d098d56b90e07984da5
Joseph Wright <joseph.wright@morningstar2.co.uk>
2009-08-01 13:34:16 +0000

    Support localinstall in MiKTeX 2.8 (which has a per-user localtexmf)

--- l3in2e/make.bat
+++ l3in2e/make.bat
@@ -377,10 +377,14 @@ set VALIDATE=..\validate
 :localinstall
 
   if not defined TEXMFHOME (
-    set TEXMFHOME=%USERPROFILE%\texmf
-    echo.
-    echo TEXMFHOME variable was not set:
-    echo using default value %USERPROFILE%\texmf
+    if exist "%APPDATA%\MiKTeX\2.8" (
+      set TEXMFHOME=%APPDATA%\MiKTeX\2.8
+    ) else (
+      echo.
+      echo TEXMFHOME variable was not set:
+      echo using default value %USERPROFILE%\texmf
+      set TEXMFHOME=%USERPROFILE%\texmf
+    )
   )
   
   SET LTEXMF=%TEXMFHOME%\tex\latex\expl3
