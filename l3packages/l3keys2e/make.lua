97b9e5ce803d8cff11cd25b935ff97a47bd1e0ed
joseph <joseph@de43f980-851b-0410-b2f7-c40aca1f87e0>
2014-05-15 06:28:31 +0000

    First version of Lua-based make scripts
    Currently these are set up for l3packages but should work OK in l3experimental
    and l3trial as well. The more complex l3kernel case needs more work, but first
    some testing for the simple versions!
    
    The approach here uses one master file for most of the code. As such, it should
    work equally well on Windows and Unix, and do the same for all of the modules/
    bundles. 
    
    Some info text needs to be added to the output yet!
    
    
    git-svn-id: http://www.latex-project.org/svnroot/experimental/trunk@4758 de43f980-851b-0410-b2f7-c40aca1f87e0

---
+++ l3packages/l3keys2e/make.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env texlua
+
+-- Make script for LaTeX3 "l3keys2e" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle  = "l3packages"
+module  = "l3keys2e"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Non-standard settings
+testfiledir  = "" -- No tests
+typesetfiles = {"*.dtx"}
+
+-- Load the common build code: this is the one place that a path needs to be
+-- hard-coded
+-- As the build system is 'self-contained' there is no module set up here: just
+--load the file in a similar way to a TeX \input
+dofile (maindir .. "/support/l3make.lua")
+
+-- Call the main function which is defined in l3make.lua
+main (arg[1], arg[2])
