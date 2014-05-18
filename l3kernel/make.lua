f023449565e90664b8f4c55d4d61adcf6287f9c9
joseph <joseph@de43f980-851b-0410-b2f7-c40aca1f87e0>
2014-05-18 15:05:32 +0000

    Add Lua script to l3kernel and adjust main scripts to fit
    This allows unpacking l3kernel into a local directory, so the tests for
    everything else do not depend on the current distribution/local installation.
    
    Note that:
     - make check is not implemented as it depends on the outcome of dicussions
       about tests in general
     - make ctan/localinstall do not currently handle l3doc.ist
     - make unpack uses only 'local' files whereas make doc and make check
       currently use local files first then the distro tree
     - Some efficiencies can probably be made with unpacking l3kernel
    
    
    git-svn-id: http://www.latex-project.org/svnroot/experimental/trunk@4791 de43f980-851b-0410-b2f7-c40aca1f87e0

---
+++ l3kernel/make.lua
@@ -0,0 +1,103 @@
+#!/usr/bin/env texlua
+
+-- Make script for LaTeX3 "l3kernel" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle  = "l3kernel"
+module  = ""
+
+-- Location of main directory: use Unix-style path separators
+maindir = ".."
+
+-- Non-standard settings
+installfiles = {"*.def", "*.cls", "*.sty", "*.tex"}
+typesetfiles =
+  {
+    "expl3.dtx", "l3docstrip.dtx", "interface3.tex", "l3syntax-changes.tex",
+    "l3styleguide.tex", "source3.tex"
+  }
+unpackfiles  = {"l3.ins"}
+
+-- Load the common build code: this is the one place that a path needs to be
+-- hard-coded
+-- As the build system is 'self-contained' there is no module set up here: just
+--load the file in a similar way to a TeX \input
+dofile (maindir .. "/support/l3make.lua")
+
+-- Unlike other parts of the build system, l3kernel has quite a number of
+-- non-standard requirements and so a number of the functions are rewritten
+-- with modification here
+-- Some of these are as simple as the fact that there is no module/bundle
+-- variation here
+
+-- There are no modules: just use the bundle name
+moduledir   = "latex/" .. bundle
+
+-- l3kernel does all of the targets itself
+function help ()
+  print ""
+  print " make check           - run automated check system       "
+  print " make checklvt <name> - check one test file <name>       "
+  print " make ctan            - create CTAN-ready archive        "
+  print " make doc             - runs all documentation files     "
+  print " make clean           - clean out directory tree         "
+  print " make localinstall    - install files in local texmf tree"
+  print " make savetlg <name>  - save test log for <name>         "
+  print " make unpack          - extract packages                 "
+  print ""
+end
+
+-- ctan () uses allmodules () to deal with each module
+-- As this is the only use relied on in l3kernel, a simple change here
+-- allows code sharing
+function allmodules (target)
+  bundlectan ()
+end
+
+-- Avoid an infinite loop
+function unpack ()
+  cleandir (unpackdir)
+  bundleunpack ()
+end
+
+-- l3kernel does all of the targets itself
+function main (target, file)
+  if target == "check" then
+    print ("Not implemented yet!")
+    -- check ()
+  elseif target == "checklvt"  then
+    if file then
+      print ("Not implemented yet!")
+      -- checklvt (file)
+    else
+      help ()
+    end
+  elseif target == "clean" then
+    clean ()
+  elseif target == "ctan" then
+    ctan ()
+  elseif target == "doc" then
+    doc ()
+  elseif target == "localinstall" then
+    localinstall ()
+  elseif target == "savetlg" then
+    if file then
+      print ("Not implemented yet!")
+      -- savetlg (file)
+    else
+      help ()
+    end
+  elseif target == "unpack" then
+    unpack ()
+    -- Additional for l3kernel: put the unpacked files somewhere known
+    for _,i in ipairs (installfiles) do
+      cp (unpackdir .. "/" .. i, localdir)
+   end
+  else
+    help ()
+  end
+end
+
+-- Call the main function which is defined in l3make.lua
+main (arg[1], arg[2])
