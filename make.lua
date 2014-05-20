2088d666a0413de309f38f3b1c0d6d1f798bbd90
joseph <joseph@de43f980-851b-0410-b2f7-c40aca1f87e0>
2014-05-20 07:58:20 +0000

    Add master Lua script for entire tree
    git-svn-id: http://www.latex-project.org/svnroot/experimental/trunk@4806 de43f980-851b-0410-b2f7-c40aca1f87e0

---
+++ make.lua
@@ -0,0 +1,71 @@
+#!/usr/bin/env texlua
+
+-- Make script for LaTeX3 files
+
+--  No bundle or module here, but these have to be defined
+bundle  = ""
+module  = ""
+
+-- A couple of custom variables: the order here is set up for 'importance'
+bundles     = {"l3kernel", "l3packages", "l3experimental"} -- l3trial to be added
+ctanbundles = {"l3kernel", "l3packages", "l3experimental"}
+
+-- Location of main directory: use Unix-style path separators
+maindir = "."
+
+-- Load the common build code: this is the one place that a path needs to be
+-- hard-coded
+-- As the build system is 'self-contained' there is no module set up here: just
+--load the file in a similar way to a TeX \input
+dofile (maindir .. "/support/l3make.lua")
+
+-- Help for the master script is simple
+function help ()
+  print ""
+  print " make check        - run automated check system       "
+  print " make ctan         - create CTAN-ready archive        "
+  print " make doc          - runs all documentation files     "
+  print " make clean        - clean out directory tree         "
+  print " make localinstall - install files in local texmf tree"
+  print " make unpack       - extract packages                 "
+  print ""
+end
+
+-- A custom main function
+function main (target)
+  local function dobundles (bundles, target)
+    local errorlevel = 0
+    for _,i in ipairs (bundles) do
+      errorlevel = run (i, "texlua make.lua " .. target)
+      if errorlevel > 0 then
+        return (errorlevel)
+      end
+    end
+    return (errorlevel)
+  end
+  if target == "check" then
+    dobundles (bundles, "check")
+  elseif target == "clean" then
+    print ("Cleaning up")
+    dobundles (bundles, "clean")
+    rm (".", "*.zip")
+  elseif target == "ctan" then
+    local errorlevel = dobundles (ctanbundles, "ctan")
+    if errorlevel == 0 then
+      for _,i in ipairs (ctanbundles) do
+        cp (i .. ".zip", i, ".")
+      end
+    end
+  elseif target == "doc" then
+    dobundles (bundles, "doc")
+  elseif target == "localinstall" then
+    dobundles (bundles, "localinstall")
+  elseif target == "unpack" then
+    dobundles (bundles, "unpack")
+  else
+    help ()
+  end
+end
+
+-- Call the main function
+main (arg[1])
