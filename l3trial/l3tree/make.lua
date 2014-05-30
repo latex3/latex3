fba9fb2651a6a7feb9605cd912dd58d451980d89
Joseph Wright <joseph.wright@morningstar2.co.uk>
2014-05-30 20:32:54 +0000

    Add Lua build system to l3trial
    This is a bit of a 'Hic sunt leones' area, so not everything is working.
    This checkin gets all of the docs to build. The following tests have
    serious issues:
    
     - cs-input: hangs due to an undefined cs (also with old build script)
     - tl-act:   hangs due to an undefined cs (also with old build script)
     - m3kernel-extras007: hangs with LuaTeX
     - m3hooks001: Fails to pass correctly even though old scripts do - some
      issue with OMIT/TIMO pairs?

---
+++ l3trial/l3tree/make.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env texlua
+
+-- Make script for LaTeX3 "l3tree" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle = "l3trail"
+module = "l3tree"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Non-standard settings
+testfiledir = ""
+
+-- Load the common build code: this is the one place that a path needs to be
+-- hard-coded
+-- As the build system is 'self-contained' there is no module set up here: just
+--load the file in a similar way to a TeX \input
+dofile (maindir .. "/support/l3make.lua")
+
+-- Call the main function which is defined in l3make.lua
+main (arg[1], arg[2], arg[3])
---
+++ l3trial/xbox/make.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env texlua
+
+-- Make script for LaTeX3 "xbox" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle = "l3trial"
+module = "xbox"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Non-standard settings
+checkdeps   = {maindir .. "/l3kernel", maindir .. "/l3packages/xparse"}
+typesetdeps = {maindir .. "/l3kernel", maindir .. "/l3packages/xparse"}
+
+-- Load the common build code: this is the one place that a path needs to be
+-- hard-coded
+-- As the build system is 'self-contained' there is no module set up here: just
+--load the file in a similar way to a TeX \input
+dofile (maindir .. "/support/l3make.lua")
+
+-- Call the main function which is defined in l3make.lua
+main (arg[1], arg[2], arg[3])
