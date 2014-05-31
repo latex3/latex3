88ecbbd314448b454894b58e9ea7072e324c6564
joseph <joseph@de43f980-851b-0410-b2f7-c40aca1f87e0>
2014-05-31 07:04:31 +0000

    Remove test files for cs-input/tl-act
    Both of these are broken in non-fixable ways from the testing perspective:
    the ideas are possibly useful so don't remove entirely.
    
    
    git-svn-id: http://www.latex-project.org/svnroot/experimental/trunk@4968 de43f980-851b-0410-b2f7-c40aca1f87e0

--- l3trial/cs-input/make.lua
+++ l3trial/cs-input/make.lua
@@ -10,6 +10,9 @@ module = "cs-input"
 -- Location of main directory: use Unix-style path separators
 maindir = "../.."
 
+-- Non-standard settings
+testfiledir = ""
+
 -- Load the common build code: this is the one place that a path needs to be
 -- hard-coded
 -- As the build system is 'self-contained' there is no module set up here: just
