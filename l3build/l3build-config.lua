3eeef360d7a93b467fec11f0d9e80ff0feac7a9a
Joseph Wright <joseph.wright@morningstar2.co.uk>
2014-06-03 18:50:37 +0000

    Enable extraction of regression-test from l3build and use as a dependency
    While there is a copy of regression-test in support, it will be used in
    preference to the extracted one. At the moment that's important as Will's
    made a small change ("2e + expl3" => "l3build") in the text, which causes
    all of the tests to fail otherwise!

--- l3build/l3build-config.lua
+++ l3build/l3build-config.lua
@@ -1,6 +1,6 @@
 -- Common settings for LaTeX3 development repo, used by l3build script
 
-checkdeps   = checkdeps   or {maindir .. "/l3kernel"}
+checkdeps   = checkdeps   or {maindir .. "/l3kernel", maindir .. "/l3build"}
 typesetdeps = typesetdeps or {maindir .. "/l3kernel"}
 unpackdeps  = unpackdeps  or {maindir .. "/l3kernel"}
 
