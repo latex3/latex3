68d860687d7fd96dfd9d0f6343bd10c03bba8f75
joseph <joseph@de43f980-851b-0410-b2f7-c40aca1f87e0>
2014-06-04 06:10:46 +0000

    Tie l3build's only script more closely to rest of L3
    Most of the settings are the same, so note the key differences.
    
    
    git-svn-id: http://www.latex-project.org/svnroot/experimental/trunk@5037 de43f980-851b-0410-b2f7-c40aca1f87e0

--- l3build/build.lua
+++ l3build/build.lua
@@ -13,12 +13,13 @@ maindir = ".."
 cleanfiles   = {"*.pdf", "*.tex", "*.zip"}
 installfiles = {"l3build.lua", "regression-test.tex"}
 sourcefiles  = {"*.dtx", "l3build.lua", "*.ins"}
-typesetcmds  = "\\AtBeginDocument{\\DisableImplementation}"
+unpackdeps   = { }
 
 -- Load the common build code: this is the one place that a path needs to be
 -- hard-coded
 -- As the build system is 'self-contained' there is no module set up here: just
 --load the file in a similar way to a TeX \input
+dofile (maindir .. "/l3build/l3build-config.lua")
 dofile (maindir .. "/l3build/l3build.lua")
 
 -- Call the main function which is defined in l3build.lua
