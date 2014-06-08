d8d3b3da195dec56bc2bf82d583554a0715f119b
Will Robertson <wspr81@gmail.com>
2014-06-08 06:50:07 +0000

    l3build: build test files updated
    * at present, can't test with multiple engines under plain
      (to be considered)
      but one engine at a time works, as shown here
    
    * cfg files work under both latex and plain

--- l3build/build-testfiles-plain.lua
+++ l3build/build-testfiles-plain.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env texlua
 
--- Testing regression suite for l3build itself
+-- Testing regression suite for l3build in plain
 
 -- Identify the bundle and module
 module = "l3build"
@@ -8,9 +8,10 @@ bundle = ""
 
 maindir = "."
 supportdir  = "."
-testfiledir = "testfiles"
+testfiledir = "testfiles-plain"
 
 installfiles = {"*.tex", "*.cfg"}
 sourcefiles  = {"*.dtx", "l3build.lua", "*.ins"}
+chkengines   = {"pdftex"}
 
 dofile ("l3build.lua")
