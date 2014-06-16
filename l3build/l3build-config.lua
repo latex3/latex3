f10bcd10a3092662d849a4c7e18fcb2ef0912be0
Joseph Wright <joseph.wright@morningstar2.co.uk>
2014-06-16 12:28:09 +0000

    l3build: Temporarily use "regression-test.tex" from support
    Something is up with the installation of deps: not sure what at present, and
    have other things to check in!

--- l3build/l3build-config.lua
+++ l3build/l3build-config.lua
@@ -6,7 +6,7 @@ unpackdeps  = unpackdeps  or {maindir .. "/l3kernel"}
 
 cmdchkfiles     = {"*.dtx"}
 checksuppfiles  =
-  {"etex.sty", "lualatexquotejobname.lua", "minimal.cls", "regression-test.cfg"}
+  {"etex.sty", "lualatexquotejobname.lua", "minimal.cls", "regression-test.cfg", "regression-test.tex"}
 unpacksuppfiles = {"docstrip.tex"}
 
 typesetcmds = "\\AtBeginDocument{\\DisableImplementation}"
