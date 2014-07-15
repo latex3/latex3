b3063849e6d6c083e2dc8b075c922a180479165b
Will Robertson <wspr81@gmail.com>
2014-07-15 10:45:42 +0000

    add l3auxdata to l3trial
    the dtx is somewhat "unconventional"...

---
+++ l3trial/l3auxdata/build.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX3 "l3benchmark" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle = "l3trial"
+module = "l3auxdata"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Non-standard settings
+testfiledir = ""
+
+-- Load the common build code: this is the one place that a path needs to be
+-- hard-coded
+-- As the build system is 'self-contained' there is no module set up here: just
+--load the file in a similar way to a TeX \input
+dofile (maindir .. "/l3build/l3build-config.lua")
+dofile (maindir .. "/l3build/l3build.lua")
