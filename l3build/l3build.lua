44d94b54e9d1a35c49bf52e0ead4763c71e18efa
Joseph Wright <joseph.wright@morningstar2.co.uk>
2014-09-15 10:48:14 +0000

    Step version (CTAN snapshot)

--- l3build/l3build.lua
+++ l3build/l3build.lua
@@ -17,8 +17,8 @@
 --]]
 
 -- Version information: should be identical to that in l3build.dtx
-release_date = "2014/08/25"
-release_ver  = "5378"
+release_date = "2014/09/15"
+release_ver  = "5423"
 
 -- "module" is a deprecated function in Lua 5.2: as we want the name
 -- for other purposes, and it should eventually be 'free', simply
