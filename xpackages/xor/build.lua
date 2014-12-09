839e1bd443501a59549c454a0fb341daed2428b9
joseph <joseph@de43f980-851b-0410-b2f7-c40aca1f87e0>
2014-12-09 22:47:37 +0000

    Add an l3build script for xor
    This is the one thing outside of the 'current' code where the tests
    really need to work. The changes here are only needed for the log2tlg to
    l3build swtich: the tests were all passing with teh old script. Note that
    we need a separate LuaTeX file at present due to the various 'out by 1' things
    going on there.
    
    Question: remove remaining batch/amke stuff, log2tlg, etc.?
    
    
    git-svn-id: http://www.latex-project.org/svnroot/experimental/trunk@5480 de43f980-851b-0410-b2f7-c40aca1f87e0

---
+++ xpackages/xor/build.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX3 "xor" files
+
+-- Identify the bundle and module
+module = "xor"
+bundle = ""
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Non-standard settings
+checkruns   = 3
+checksearch = true
+
+-- Load the common build code: this is the one place that a path needs to be
+-- hard-coded
+-- As the build system is 'self-contained' there is no module set up here: just
+--load the file in a similar way to a TeX \input
+dofile (maindir .. "/l3build/l3build-config.lua")
+dofile (maindir .. "/l3build/l3build.lua")
