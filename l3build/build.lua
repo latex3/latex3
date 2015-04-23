eee5a3ef255a7c9e0dfacd13c6aa73ecaa3800ab
Joseph Wright <joseph.wright@morningstar2.co.uk>
2015-04-23 07:31:15 +0000

    Missed one file

--- l3build/build.lua
+++ l3build/build.lua
@@ -20,5 +20,5 @@ unpackdeps   = { }
 -- hard-coded
 -- As the build system is 'self-contained' there is no module set up here: just
 --load the file in a similar way to a TeX \input
-dofile (maindir .. "/l3build/l3build-config.lua")
+dofile (maindir .. "/build-config.lua")
 dofile (maindir .. "/l3build/l3build.lua")
