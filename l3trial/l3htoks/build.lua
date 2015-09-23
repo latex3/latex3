e440df73b0485e3e06db238ae7ef774d84df384a
Joseph Wright <joseph.wright@morningstar2.co.uk>
2015-09-23 21:54:47 +0100

    Allow Travis to integrate with Gitter

--- l3trial/l3htoks/build.lua
+++ l3trial/l3htoks/build.lua
@@ -11,8 +11,9 @@ module = "l3htoks"
 maindir = "../.."
 
 -- Non-standard settings
-checkruns   = 2
-checksearch = true
+checkruns    = 2
+checkengines = {"pdftex", "xetex", "luatex"}
+checksearch  = true
 
 -- Load the common build code: this is the one place that a path needs to be
 -- hard-coded
