aa9195176a8db81daaa21b1a23323ee9da3938a9
Joseph Wright <joseph.wright@morningstar2.co.uk>
2015-10-08 13:56:28 +0100

    Allow for l3build.lua itself in version setting

--- l3build/build.lua
+++ l3build/build.lua
@@ -15,6 +15,33 @@ cleanfiles   = {"*.pdf", "*.tex", "*.zip"}
 installfiles = {"l3build.lua", "regression-test.tex"}
 sourcefiles  = {"*.dtx", "l3build.lua", "*.ins"}
 unpackdeps   = { }
+versionfiles = {"*.dtx", "*.md", "l3build.lua"}
+
+-- Detail how to set the version automatically
+function setversion_update_line(line, date, version)
+  local date = string.gsub(date, "%-", "/")
+  -- Replace the identifiers in .dtx files
+  if string.match(line, "^\\def\\ExplFileDate{%d%d%d%d/%d%d/%d%d}$") then
+    line = "\\def\\ExplFileDate{" .. date .. "}"
+  end
+  if string.match(line, "^\\def\\ExplFileVersion{%d+}$") then
+    line = "\\def\\ExplFileVersion{" .. version .. "}"
+  end
+  -- Markdown files
+  if string.match(
+    line, "^Release %d%d%d%d/%d%d/%d%d %(r%d%d%d%d%)$"
+  ) then
+    line = "Release " .. date .. " (r" .. version .. ")"
+  end
+  -- l3build.lua
+  if string.match(line, "^release_date = \"%d%d%d%d/%d%d/%d%d\"$") then
+    line = "release_date = \"" .. date .. "\""
+  end
+  if string.match(line, "^release_ver  = \"%d%d%d%d\"$") then
+    line = "release_ver  = \"" .. version .. "\""
+  end
+  return line
+end
 
 -- Load the common build code: this is the one place that a path needs to be
 -- hard-coded
