00cda3342d1f3b29dff52203e984fbeced6a1892
Joseph Wright <joseph.wright@morningstar2.co.uk>
2017-10-10 22:05:53 +0100

    build.lua file for colour additions

---
+++ l3trial/l3color-addn/build.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX3 "l3color-addn" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle = "l3trial"
+module = "l3color-addn"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Load the common build code
+dofile(maindir .. "/build-config.lua")
+
+-- Find and run the build system
+kpse.set_program_name("kpsewhich")
+dofile(kpse.lookup("l3build.lua"))
