dd028dd1006368ab54c2e18028dff1d5c3b02a70
Joseph Wright <joseph.wright@morningstar2.co.uk>
2017-10-13 19:47:24 +0100

    s/color-extra/color-extras/
    Lines up with Bruno's approach.

--- l3trial/l3color-extras/build.lua
+++ l3trial/l3color-extras/build.lua
@@ -1,11 +1,11 @@
 #!/usr/bin/env texlua
 
--- Build script for LaTeX3 "l3color-extra" files
+-- Build script for LaTeX3 "l3color-extras" files
 
 -- Identify the bundle and module: the module may be empty in the case where
 -- there is no subdivision
 bundle = "l3trial"
-module = "l3color-extra"
+module = "l3color-extras"
 
 -- Location of main directory: use Unix-style path separators
 maindir = "../.."
