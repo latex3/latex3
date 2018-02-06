6e68728f7822c05c7f94de5e5b2000f3c7eb130e
Joseph Wright <joseph.wright@morningstar2.co.uk>
2018-02-06 20:56:40 +0000

    Initialise l3draw work
    This is a stub at present, with the idea being to implement something
    more-or-less the same as pgf (the base layer of TikZ). The key
    difference between the pgf code and the plan here is that we have the
    FPU to build on, which should provide much more even handling of e.g.
    small numbers.
    
    The plan is to stay interface-compatible with pgf if possible: it's well
    thought-out but also that may allow 'drop in' TikZ compatiblity level.
    (There are a few places some patching might be required.)
    
    As pgf is a mature and complex system, the aim is to build up part by
    part. In particular, some more tricky features will likely be missing
    for some time. This is already evident in the driver-level code added to
    l3kernel: at present, this doesn't have pgf's 'protocol' (buffering)
    approach to paths.
    
    The initial files follow the structure of pgfcore, though the plan at
    present is to combine all of the path functions into a single (slightly
    long) source.
    
    Getting to a working set up will require work in various places: there
    will be a number of commits to get anywhere near that. See also above
    concerning the fact that some code will be simplified, particularly in a
    first pass.

---
+++ l3trial/l3draw/build.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX3 "l3draw" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle = "l3trial"
+module = "l3draw"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Load the common build code
+dofile(maindir .. "/build-config.lua")
+
+-- Find and run the build system
+kpse.set_program_name("kpsewhich")
+dofile(kpse.lookup("l3build.lua"))
