fe6fea5a16c2738f85c6ee7c70103c5e4ec71163
Joseph Wright <joseph.wright@morningstar2.co.uk>
2018-09-14 11:18:17 +0100

    Move xmarks to 'active' part of tree
    No changes/fixes to the code just yet: this is a straight copy-paste.

---
+++ l3experimental/xmarks/build.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX3 "xmarks" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle = "l3experimental"
+module = "xmarks"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Non-standard settings
+checkdeps = {maindir .. "/l3packages/xparse"}
+
+-- Load the common build code
+dofile(maindir .. "/build-config.lua")
+
+-- Find and run the build system
+kpse.set_program_name("kpsewhich")
+if not release_date then
+  dofile(kpse.lookup("l3build.lua"))
+end
+
