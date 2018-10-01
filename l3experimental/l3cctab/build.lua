4f1320f8eae9a96d11fc0f35d44965ca196dbe4b
Joseph Wright <joseph.wright@morningstar2.co.uk>
2018-10-01 07:54:40 +0100

    Initialise l3cctab using code from old l3luatex (see #470)
    This is more-or-less the code from the older, rather unstable,
    LuaTeX module, but with minor updates to get it working following
    expl3 changes.
    
    Support for other engines to be added later, plus need to explore
    some kind of 'save current catcodes' approach.

---
+++ l3experimental/l3cctab/build.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX3 "l3cctab" files
+
+-- Identify the bundle and module: the module may be empty in the case where
+-- there is no subdivision
+bundle = "l3experimental"
+module = "l3cctab"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Load the common build code
+dofile(maindir .. "/build-config.lua")
+
+-- Find and run the build system
+kpse.set_program_name("kpsewhich")
+if not release_date then
+  dofile(kpse.lookup("l3build.lua"))
+end
+
