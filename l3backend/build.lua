fd9b6bb2e9d5017e1864ac3e446ff0993b5d5e4a
Joseph Wright <joseph.wright@morningstar2.co.uk>
2020-02-03 19:54:33 +0000

    Correct tagging of backend files

--- l3backend/build.lua
+++ l3backend/build.lua
@@ -46,9 +46,9 @@ function update_tag(file,content,tagname,tagdate)
      year - 2 .. "," .. year - 1 .. "," .. year,
      year - 2 .. "-" .. year)
   end
-  if string.match(file,"l3backend-basics%.dtx$") then
+  if string.match(file,"l3backend%-basics%.dtx$") then
     content = string.gsub(content,
-      "\n  ({l3%w+%.def}){" .. iso .. "}",
+      "\n  ({l3backend%-%w+%.def}){" .. iso .. "}",
       "\n  %1{" .. tagname .. "}")
   end
   if string.match(file,"%.dtx$") or string.match(file,"%.tex$") then
