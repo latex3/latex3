6ff9262ae0e94f21ed6b5bf190e18c630dbefc59
Joseph Wright <joseph.wright@morningstar2.co.uk>
2020-05-12 10:17:06 +0100

    Use 'xparse-generic' as core name

--- l3packages/xparse/build.lua
+++ l3packages/xparse/build.lua
@@ -7,7 +7,7 @@
 bundle = "l3packages"
 module = "xparse"
 
-installfiles = {"xparse.ltx", "xparse.sty", "xparse.tex"}
+installfiles = {"xparse.ltx", "xparse.sty", "xparse-generic.tex"}
 
 -- Location of main directory: use Unix-style path separators
 maindir = "../.."
