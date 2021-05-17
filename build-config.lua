3c326e14b222ee4e2929c5ba0618880240a1771e
Marcel Fabian Krüger <tex@2krueger.de>
2021-05-17 05:01:22 +0200

    Only build necessary format during `l3build save`

--- build-config.lua
+++ build-config.lua
@@ -141,7 +141,18 @@ local function fmt(engines,dest)
 end
 
 function checkinit_hook()
-  return fmt(options["engine"] or checkengines,testdir)
+  local engines = options.engine
+  if not engines then
+    local target = options.target
+    if target == 'check' then
+      engines = checkengines
+    elseif target == 'save' then
+      engines = {stdengine}
+    else
+      error'Unexpected target in call to checkinit_hook'
+    end
+  end
+  return fmt(engines,testdir)
 end
 
 function docinit_hook()
