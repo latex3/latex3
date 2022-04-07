b315efba492ff072f0d0a52df3e9ad13083b3fde
Joseph Wright <joseph.wright@morningstar2.co.uk>
2022-04-07 15:25:49 +0100

    Extend testing to cover backend features

---
+++ l3kernel/config-backend.lua
@@ -0,0 +1,19 @@
+testfiledir  = "testfiles-backend"
+
+-- Set up to allow testing dvips, etc.
+specialformats = specialformats or {}
+specialformats.latex =
+  {
+    luatex = {binary = "luahbtex",format = "lualatex"},
+    ptex  = {binary = "eptex"},
+    uptex = {binary = "euptex"},
+    ["etex-dvips"] = {binary = "etex", format = "latex"},
+    ["etex-dvisvgm"] =
+      {
+        binary = "etex",
+        format = "latex",
+        tokens = "\\PassOptionsToPackage{backend=dvisvgm}{expl3}"
+      }
+  }
+checkengines =
+  {"pdftex", "luatex", "xetex", "etex-dvips", "etex-dvisvgm", "ptex", "uptex"}
