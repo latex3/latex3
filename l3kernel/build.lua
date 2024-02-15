latex3/latex3

commit ecccd76a5d5e965bc7a428d2e6c72873724070ae

Avoid code duplicate in tagging routine
By adding a hook, we can cover the extra for l3kernel without copy-pasting the entire block.

l3kernel/build.lua
@@ -51,52 +51,14 @@ typesetdeps = typesetdeps
 dofile(maindir .. "/build-config.lua")
 
 -- Detail how to set the version automatically
-function update_tag(file,content,tagname,tagdate)
+function update_tag_extra(file,content,tagname,tagdate)
   local iso = "%d%d%d%d%-%d%d%-%d%d"
-  local url = "https://github.com/latex3/latex3/compare/"
-  if string.match(content,"%(C%)%s*[%d%-,]+ The LaTeX Project") then
-    local year = os.date("%Y")
-    content = string.gsub(content,
-      "%(C%)%s*([%d%-,]+) The LaTeX Project",
-      "(C) %1," .. year .. " The LaTeX Project")
-   content = string.gsub(content,year .. "," .. year,year)
-   content = string.gsub(content,
-     "%-" .. tonumber(year) - 1 .. "," .. year,
-     "-" .. year)
-   content = string.gsub(content,
-     tonumber(year) - 2 .. "," .. tonumber(year) - 1 .. "," .. year,
-     tonumber(year) - 2 .. "-" .. year)
-  end
   if string.match(file,"expl3%.dtx$") then
     content = string.gsub(content,
       "\n\\def\\ExplFileDate{" .. iso .. "}%%\n",
       "\n\\def\\ExplFileDate{" .. tagname .. "}%%\n")
-  elseif string.match(file,"l3debug%.dtx$") then
-    content = string.gsub(content,
-      "\n\\ProvidesExplFile{l3debug%.def}{" .. iso .. "}",
-      "\n\\ProvidesExplFile{l3debug.def}{" .. tagname .. "}")
   end
-  if string.match(file,"%.dtx$") or string.match(file,"%.tex$") then
-    return string.gsub(content,
-      "\n(%%*%s*)\\date{Released " .. iso .. "}\n",
-      "\n%1\\date{Released " .. tagname .. "}\n")
-  elseif string.match(file, "%.md$") then
-    if string.match(file,"CHANGELOG.md") then
-      local previous = string.match(content,"compare/(" .. iso .. ")%.%.%.HEAD")
-      if tagname == previous then return content end
-      content = string.gsub(content,
-        "## %[Unreleased%]",
-        "## [Unreleased]\n\n## [" .. tagname .."]")
-      return string.gsub(content,
-        iso .. "%.%.%.HEAD",
-        tagname .. "...HEAD\n[" .. tagname .. "]: " .. url .. previous
-          .. "..." .. tagname)
-    end
-    return string.gsub(content,
-      "\nRelease " .. iso .. "\n",
-      "\nRelease " .. tagname .. "\n")
-  end
-  return content
+  return(content)
 end
 
 uploadconfig = {

build-config.lua
@@ -44,6 +44,7 @@ end
 function update_tag(file,content,tagname,tagdate)
   local iso = "%d%d%d%d%-%d%d%-%d%d"
   local url = "https://github.com/latex3/latex3/compare/"
+  content =  update_tag_extra(file,content,tagname,tagdate)
   if string.match(content,"%(C%)%s*[%d%-,]+ The LaTeX Project") then
     local year = os.date("%Y")
     content = string.gsub(content,
@@ -81,6 +82,10 @@ function update_tag(file,content,tagname,tagdate)
       "\nRelease " .. tagname .. "\n")
   end
   return content
+end
+
+function update_tag_extra(file,content,tagname,tagdate)
+  return(content)
 end
 
 -- Need to build format files

commit 5076c9d64777185c4a135fb3155a57a40cc6b0c3

Fix tagging for tex files
This fixes a regression introduced in ecccd76a (Avoid code duplicate in tagging
routine, 2023-12-11) that made tex files in `./l3kernel/doc` not tagged.

l3kernel/build.lua
@@ -57,6 +57,10 @@ function update_tag_extra(file,content,tagname,tagdate)
     content = string.gsub(content,
       "\n\\def\\ExplFileDate{" .. iso .. "}%%\n",
       "\n\\def\\ExplFileDate{" .. tagname .. "}%%\n")
+  elseif string.match(file,"%.tex$") then
+    content = string.gsub(content,
+      "\n\\date{Released " .. iso .. "}\n",
+      "\n\\date{Released " .. tagname .. "}\n")
   end
   return(content)
 end

l3kernel/doc/interface3.tex
@@ -58,7 +58,7 @@ for those people who are interested.
          {latex-team@latex-project.org}%
    }%
 }
-\date{Released 2023-12-08}
+\date{Released 2024-02-13}
 
 \pagenumbering{roman}
 \maketitle

l3kernel/doc/l3styleguide.tex
@@ -32,7 +32,7 @@ The released version of this bundle is available from CTAN.
         {latex-team@latex-project.org}%
     }%
 }
-\date{Released 2023-12-08}
+\date{Released 2024-02-13}
 
 \begin{document}
 

l3kernel/doc/l3term-glossary.tex
@@ -32,7 +32,7 @@ The released version of this bundle is available from CTAN.
         {latex-team@latex-project.org}%
     }%
 }
-\date{Released 2023-12-08}
+\date{Released 2024-02-13}
 
 \newcommand{\TF}{\textit{(TF)}}
 

l3kernel/doc/source3.tex
@@ -57,7 +57,7 @@ for those people who are interested.
          {latex-team@latex-project.org}%
    }%
 }
-\date{Released 2023-12-08}
+\date{Released 2024-02-13}
 
 \pagenumbering{roman}
 \maketitle