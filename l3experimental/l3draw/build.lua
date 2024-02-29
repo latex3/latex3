1e3c923c8afbd4336eb47d9b42065e809589ab0e
Yukai Chou <muzimuzhi@gmail.com>
2024-03-01 07:19:01 +0800

    Move specialformats settings to root config

--- l3experimental/l3draw/build.lua
+++ l3experimental/l3draw/build.lua
@@ -12,21 +12,6 @@ maindir = "../.."
 
 typesetfiles = {"l3draw.dtx", "l3draw-code.tex"}
 
--- Set up to allow testing dvips, etc.
-specialformats = specialformats or {}
-specialformats.latex =
-  {
-    luatex = {binary = "luahbtex",format = "lualatex"},
-    ptex  = {binary = "eptex"},
-    uptex = {binary = "euptex"},
-    ["etex-dvips"] = {binary = "etex", format = "latex"},
-    ["etex-dvisvgm"] =
-      {
-        binary = "etex",
-        format = "latex",
-        tokens = "\\PassOptionsToPackage{backend=dvisvgm}{expl3}"
-      }
-  }
 checkengines =
   {"pdftex", "luatex", "xetex", "etex-dvips", "etex-dvisvgm", "uptex"}
 
--- l3experimental/l3graphics/build.lua
+++ l3experimental/l3graphics/build.lua
@@ -10,21 +10,6 @@ module = "l3graphics"
 -- Location of main directory: use Unix-style path separators
 maindir = "../.."
 
--- Set up to allow testing dvips, etc.
-specialformats = specialformats or {}
-specialformats.latex =
-  {
-    luatex = {binary = "luahbtex",format = "lualatex"},
-    ptex  = {binary = "eptex"},
-    uptex = {binary = "euptex"},
-    ["etex-dvips"] = {binary = "etex", format = "latex"},
-    ["etex-dvisvgm"] =
-      {
-        binary = "etex",
-        format = "latex",
-        tokens = "\\PassOptionsToPackage{backend=dvisvgm}{expl3}"
-      }
-  }
 checkengines =
   {"pdftex", "luatex", "xetex", "etex-dvips", "etex-dvisvgm", "uptex"}
 
