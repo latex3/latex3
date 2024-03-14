f12202f1aef79d8682ef4cfe32b0e2623391d39e
Joseph Wright <joseph@texdev.net>
2024-03-14 07:44:04 +0000

    Try again ...

--- l3kernel/config-l3doc.lua
+++ l3kernel/config-l3doc.lua
@@ -1,5 +1,5 @@
 testfiledir  = "testfiles-l3doc"
-checkdeps    = {maindir .. "/l3backend", maindir .. "/l3packages"}
+checkdeps    = {maindir .. "/l3packages/l3keys2e"}
 checkengines = { "pdftex" }
 checksearch  = true
 checkruns    = 3
