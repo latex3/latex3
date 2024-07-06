aa551d052a84a3836a131c237a4c2b0770e0fe29
Max Chernoff <49086429+gucci-on-fleek@users.noreply.github.com>
2024-07-06 03:08:32 -0600

    Add support for local property lists

--- lua-tests/expl-lua.lua
+++ lua-tests/expl-lua.lua
@@ -5,9 +5,8 @@
 -- Save some locals for a slight speed boost.
 local append              = table.append
 local concatenate         = table.concat
-local direct_get_data     = node.direct.getdata
-local direct_set_data     = node.direct.setdata
-local direct_set_field    = node.direct.setfield
+local create_token        = token.create
+local getmetatable        = getmetatable
 local insert              = table.insert
 local ipairs              = ipairs
 local lua_functions_table = lua.get_functions_table()
@@ -16,21 +15,25 @@ local merge_hash          = table.merged
 local new_lua_function    = luatexbase.new_luafunction
 local new_token           = token.new
 local next                = next
-local node                = node
 local pairs               = pairs
 local scan_argument       = token.scan_argument
 local scan_csname         = token.scan_csname
+local scan_expanded       = token.scan_token
 local scan_int            = token.scan_int
 local scan_string         = token.scan_string
-local scan_token          = token.scan_token
+local scan_token          = token.get_next
 local scan_toks           = token.scan_toks
 local setmetatable        = setmetatable
+local sorted_pairs        = table.sortedpairs
 local sprint              = tex.sprint
+local tex_get             = tex.get
 local token               = token
-local token_get_next      = token.get_next
-local token_put_next      = token.put_next
+local token_get_csname    = token.get_csname
+local token_get_mode      = token.get_mode
+local token_set_char      = token.set_char
 local token_set_lua       = token.set_lua
 local unpack              = table.unpack
+local write_token         = token.put_next
 
 
 ------------------------
@@ -138,28 +141,6 @@ local function define_macro(params)
 end
 
 
---- @class (exact) tab_tok: table A table representing a token.
---- @field [1] integer      (cmd) The command code of the token.
---- @field [2] integer      (chr) The character code of the token.
---- @field [3] integer?     (cs)  The index into the \TeX{} hash table.
-
-local token_userdata_to_table
-do
-    local scratch = node.direct.new("whatsit", "user_defined")
-    direct_set_field(scratch, "type", ("t"):byte())
-
-    --- Converts a table of “userdata” tokens into a table of triplets, each
-    --- representing a token.
-    ---
-    --- @param  userdata  luatex.token[]
-    --- @return tab_tok[] table
-    function token_userdata_to_table(userdata)
-        direct_set_data(scratch, userdata)
-        return direct_get_data(scratch)
-    end
-end
-
-
 --- @class _memoized<K, V>: { [K]: V } A memoized table.
 --- @operator call(any): any           - -
 
@@ -192,59 +173,172 @@ end)
 --- Property List Functions ---
 -------------------------------
 
+-- Module-local storage
 local prop_storage = {}
+local MIN_PROP_ID = 127
+local max_prop_id = MIN_PROP_ID
 
+-- Utility functions
+local function new_prop(name)
+    max_prop_id = max_prop_id + 1
+
+    local id = max_prop_id
+    local current_level = 0 -- tex_get("currentgrouplevel")
+
+    prop_storage[id] = {}
+    prop_storage[id][current_level] = {}
+
+    local packed = (id << 16) | current_level
+
+    token_set_char(name, packed, "global")
+end
+
+local function prop_meta_pairs(level)
+    local meta = getmetatable(level)
+    local entry = prop_storage[meta.id]
+
+    local current_level = tex_get("currentgrouplevel")
+    local all_levels = {}
+    for i = 0, current_level do
+        all_levels[#all_levels+1] = entry[i]
+    end
+
+    local merged = merge_hash(unpack(all_levels))
+    return next, merged
+end
+
+local function new_prop_level(token, value)
+    local packed = token_get_mode(token)
+    local id = packed >> 16
+    local tex_level = packed & 0xFFFF
+
+    local entry = prop_storage[id]
+    local current_level = tex_get("currentgrouplevel")
+
+    if tex_level == current_level then
+        return entry[tex_level]
+    else
+        local new_level = setmetatable(value or {}, {
+            __index = entry[tex_level],
+            __pairs = prop_meta_pairs,
+            id      = id,
+        })
+        local csname = token_get_csname(token)
+        local new_packed = (id << 16) | current_level
+
+        token_set_char(csname, new_packed)
+        entry[current_level] = new_level
+
+        return new_level
+    end
+end
+
+local function get_prop_level(token, forced_level, new_value)
+    local packed = token_get_mode(token)
+    local id = packed >> 16
+    local tex_level = forced_level or (packed & 0xFFFF)
+
+    if id < MIN_PROP_ID then
+        return {}
+    end
+
+    if new_value then
+        prop_storage[id][tex_level] = new_value
+    else
+        for i = tex_level, 0, -1 do
+            local level = prop_storage[id][i]
+            if level then
+                return level
+            end
+        end
+    end
+end
+
+-- TeX functions
 define_macro {
     module      = "lprop",
     name        = "new",
     arguments   = { "csname", },
     visibility  = "private",
-    func = function(name)
-        prop_storage[name] = {}
+    func = new_prop,
+}
+
+define_macro {
+    module     = "lprop",
+    name       = "put",
+    arguments  = { "token", "string", "toks", },
+    visibility = "public",
+    no_scan    = true,
+    func = function()
+        local prop  = scan_token()
+        local key   = scan_string()
+        local value = scan_toks()
+
+        local level = new_prop_level(prop)
+        level[key] = value
     end,
 }
 
 define_macro {
     module     = "lprop",
     name       = "gput",
-    arguments  = { "csname", "string", "toks", },
+    arguments  = { "token", "string", "toks", },
     visibility = "public",
     no_scan    = true,
     func = function()
-        prop_storage[scan_csname()][scan_string()] = scan_toks()
+        local prop  = scan_token()
+        local key   = scan_string()
+        local value = scan_toks()
+
+        local level = get_prop_level(prop, 0)
+        level[key] = value
     end,
 }
 
 define_macro {
     module     = "lprop",
     name       = "item",
-    arguments  = { "csname", "string", },
+    arguments  = { "token", "string", },
     visibility = "public",
     no_scan    = true,
     func = function()
-        token_put_next(prop_storage[scan_csname()][scan_string()] or {})
+        local prop = scan_token()
+        local key  = scan_string()
+
+        local level = get_prop_level(prop)
+        local value = level[key]
+
+        if value then
+            write_token(value)
+        end
+    end,
+}
+
+define_macro {
+    module     = "lprop",
+    name       = "clear",
+    arguments  = { "token", },
+    visibility = "public",
+    no_scan    = true,
+    func = function()
+        local prop = scan_token()
+        local level = new_prop_level(prop)
+
+        for key in pairs(level) do
+            level[key] = false
+        end
     end,
 }
 
 define_macro {
     module     = "lprop",
     name       = "gclear",
-    arguments  = { "csname", },
+    arguments  = { "token", },
     visibility = "public",
     no_scan    = true,
     func = function()
-        prop_storage[scan_csname()] = {}
-    end,
-}
-
-define_macro {
-    module     = "lprop",
-    name       = "gset_eq",
-    arguments  = { "csname", "csname", },
-    visibility = "public",
-    no_scan    = true,
-    func = function()
-        prop_storage[scan_csname()] = prop_storage[scan_csname()]
+        local prop = scan_token()
+        get_prop_level(prop, 0, {})
     end,
 }
 
@@ -255,93 +349,113 @@ local END_GROUP            = command_id["right_brace"]
 local SPACER               = command_id["spacer"]
 local value_terminator_tok = new_token(VALUE_TERMINATOR, command_id["other"])
 
+local function parse_keyval(level)
+    local keyval_toks = scan_toks()
+    keyval_toks[#keyval_toks + 1] = value_terminator_tok
+
+    local nest = 0
+    local key, value = {}, {}
+    local target_key = true
+
+    for i = 1, #keyval_toks do
+        local user_tok = keyval_toks[i]
+        local cmd = user_tok.command
+        local chr = user_tok.mode
+        local cs  = user_tok.csname
+
+        -- Always insert macros
+        if cs then
+            goto insert_token
+        end
+
+        -- Handle nesting
+        if cmd == BEGIN_GROUP then
+            nest = nest + 1
+        elseif cmd == END_GROUP then
+            nest = nest - 1
+        end
+
+        -- Only parse the key-value pair if we're at the outermost level
+        if nest == 0 then
+            -- Store the key-value pair
+            if chr == VALUE_TERMINATOR then
+                level[concatenate(key)] = value
+                key, value = {}, {}
+                target_key = true
+                goto continue
+
+            -- End the key and switch to the value
+            elseif chr == KEY_TERMINATOR then
+                target_key = false
+                goto continue
+
+            -- Trim spaces
+            elseif cmd == SPACER then
+                -- Trim spaces at the beginning
+                if target_key and (#key == 0) then
+                    goto continue
+                elseif (not target_key) and (#value == 0) then
+                    goto continue
+                end
+
+                -- Trim spaces at the end
+                local next_chr = (keyval_toks[i + 1] or {}).mode
+                if (not next_chr) or
+                    (next_chr == VALUE_TERMINATOR) or
+                    (next_chr == KEY_TERMINATOR)
+                then
+                    goto continue
+                end
+
+            -- Skip outermost closing brace
+            elseif cmd == END_GROUP then
+                goto continue
+            end
+
+        -- Skip the outermost opening brace
+        elseif nest == 1 then
+            if cmd == BEGIN_GROUP then
+                goto continue
+            end
+        end
+
+        -- Insert the token
+        ::insert_token::
+        if target_key then
+            key[#key + 1] = codepoint_to_char[chr]
+        else
+            value[#value + 1] = user_tok
+        end
+
+        ::continue::
+    end
+end
+
 define_macro {
     module     = "lprop",
-    name       = "gput_from_keyval",
-    arguments  = { "csname", "toks", },
+    name       = "put_from_keyval",
+    arguments  = { "token", "toks", },
     visibility = "public",
     no_scan    = true,
     func = function()
-        local prop = prop_storage[scan_csname()]
-        local user_toks = scan_toks()
-        user_toks[#user_toks + 1] = value_terminator_tok
+        local prop  = scan_token()
+        local level = new_prop_level(prop)
 
-        local nest = 0
-        local key, value = {}, {}
-        local target_key = true
+        parse_keyval(level)
+    end,
+}
 
-        for i = 1, #user_toks do
-            local user_tok = user_toks[i]
-            local cmd = user_tok.command
-            local chr = user_tok.mode
-            local cs  = user_tok.csname
+define_macro {
+    module     = "lprop",
+    name       = "gput_from_keyval",
+    arguments  = { "token", "toks", },
+    visibility = "public",
+    no_scan    = true,
+    func = function()
+        local prop = scan_token()
+        local level = get_prop_level(prop, 0)
 
-            -- Always insert macros
-            if cs then
-                goto insert_token
-            end
-
-            -- Handle nesting
-            if cmd == BEGIN_GROUP then
-                nest = nest + 1
-            elseif cmd == END_GROUP then
-                nest = nest - 1
-            end
-
-            -- Only parse the key-value pair if we're at the outermost level
-            if nest == 0 then
-                -- Store the key-value pair
-                if chr == VALUE_TERMINATOR then
-                    prop[concatenate(key)] = value
-                    key, value = {}, {}
-                    target_key = true
-                    goto continue
-
-                -- End the key and switch to the value
-                elseif chr == KEY_TERMINATOR then
-                    target_key = false
-                    goto continue
-
-                -- Trim spaces
-                elseif cmd == SPACER then
-                    -- Trim spaces at the beginning
-                    if target_key and (#key == 0) then
-                        goto continue
-                    elseif (not target_key) and (#value == 0) then
-                        goto continue
-                    end
-
-                    -- Trim spaces at the end
-                    local next_chr = (user_toks[i + 1] or {}).mode
-                    if (not next_chr) or
-                       (next_chr == VALUE_TERMINATOR) or
-                       (next_chr == KEY_TERMINATOR)
-                    then
-                        goto continue
-                    end
-
-                -- Skip outermost closing brace
-                elseif cmd == END_GROUP then
-                    goto continue
-                end
-
-            -- Skip the outermost opening brace
-            elseif nest == 1 then
-                if cmd == BEGIN_GROUP then
-                    goto continue
-                end
-            end
-
-            -- Insert the token
-            ::insert_token::
-            if target_key then
-                key[#key + 1] = codepoint_to_char[chr]
-            else
-                value[#value + 1] = user_tok
-            end
-
-            ::continue::
-        end
+        parse_keyval(level)
     end,
 }
 
@@ -351,23 +465,26 @@ local end_group_tok = new_token(char_to_codepoint["}"], END_GROUP)
 define_macro {
     module     = "lprop",
     name       = "map_function",
-    arguments  = { "csname", "csname", },
+    arguments  = { "token", "token", },
     visibility = "public",
     no_scan    = true,
     func = function()
-        local prop = prop_storage[scan_csname()]
-        local func = token_get_next()
+        local prop  = scan_token()
+        local level = get_prop_level(prop)
+        local func  = scan_token()
 
         local toks, len = {}, 0
-        for key, value in next, prop do
-            toks[len + 1] = func
-            toks[len + 2] = begin_group_tok
-            toks[len + 3] = key
-            toks[len + 4] = end_group_tok
-            toks[len + 5] = begin_group_tok
-            append(toks, value)
-            len = #toks + 1
-            toks[len + 0] = end_group_tok
+        for key, value in pairs(level) do
+            if value then
+                toks[len + 1] = func
+                toks[len + 2] = begin_group_tok
+                toks[len + 3] = key
+                toks[len + 4] = end_group_tok
+                toks[len + 5] = begin_group_tok
+                append(toks, value)
+                len = #toks + 1
+                toks[len + 0] = end_group_tok
+            end
         end
 
         sprint(-2, toks)
@@ -376,36 +493,72 @@ define_macro {
 
 define_macro {
     module     = "lprop",
-    name       = "gconcat",
-    arguments  = { "csname", "csname", "csname", },
+    name       = "concat",
+    arguments  = { "token", "token", "token", },
     visibility = "public",
     func = function(out, first, second)
-        prop_storage[out] = merge_hash(prop_storage[first], prop_storage[second])
+        local first_level = get_prop_level(first)
+        local second_level = get_prop_level(second)
+        new_prop_level(out, merge_hash(first_level, second_level))
+    end,
+}
+
+define_macro {
+    module     = "lprop",
+    name       = "gconcat",
+    arguments  = { "token", "token", "token", },
+    visibility = "public",
+    func = function(out, first, second)
+        local first_level = get_prop_level(first, 0)
+        local second_level = get_prop_level(second, 0)
+        get_prop_level(out, 0, merge_hash(first_level, second_level))
     end,
 }
 
 define_macro {
     module     = "lprop",
     name       = "count",
-    arguments  = { "csname", },
+    arguments  = { "token", },
     visibility = "public",
     no_scan    = true,
     func = function()
+        local prop  = scan_token()
+        local level = get_prop_level(prop)
+
         local count = 0
-        for _ in next, prop_storage[scan_csname()] do
+        for _ in pairs(level) do
             count = count + 1
         end
-        token_put_next(to_chardef[count])
+        write_token(to_chardef[count])
+    end,
+}
+
+define_macro {
+    module     = "lprop",
+    name       = "remove",
+    arguments  = { "token", "string", },
+    visibility = "public",
+    no_scan    = true,
+    func = function()
+        local prop  = scan_token()
+        local key   = scan_string()
+        local level = new_prop_level(prop)
+
+        level[key] = false
     end,
 }
 
 define_macro {
     module     = "lprop",
     name       = "gremove",
-    arguments  = { "csname", "string", },
+    arguments  = { "token", "string", },
     visibility = "public",
     no_scan    = true,
     func = function()
-        prop_storage[scan_csname()][scan_string()] = nil
+        local prop  = scan_token()
+        local key   = scan_string()
+        local level = get_prop_level(prop, 0)
+
+        level[key] = nil
     end,
 }
